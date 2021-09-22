f7ca7abf6126cd9354d033ad144efaa495ea95aa
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-09-22 14:52:11 -0300

    add tables

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   public titulo = 'Alunos';
 
   public alunos = [
-  { nome: 'Marta', },
-  { nome: 'Paula', },
-  { nome: 'Laura', },
-  { nome: 'Luiza', },
-  { nome: 'Lucas', },
-  { nome: 'Pedro', },
-  { nome: 'Paulo', },
+  { id:1, nome: 'Marta',sobrenome: 'Kent', telefone: 332255 },
+  { id:2, nome: 'Paula',sobrenome: 'Isabela', telefone: 33412 },
+  { id:3, nome: 'Laura',sobrenome: 'Antonia', telefone: 221155 },
+  { id:4, nome: 'Luiza',sobrenome: 'Maria', telefone: 113344 },
+  { id:5, nome: 'Lucas',sobrenome: 'Machado', telefone: 433254 },
+  { id:6, nome: 'Pedro',sobrenome: 'Alvares', telefone: 512255 },
+  { id:7, nome: 'Paulo',sobrenome: 'José', telefone: 132151 },
   ]
 
   constructor() { }
--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -10,11 +10,11 @@ export class ProfessoresComponent implements OnInit {
   titulo = 'Professores';
 
   public professores = [
-    { nome :'Lauro'    ,}, 
-    { nome :'Roberto'  ,},
-    { nome :'Ronaldo',},
-    { nome :'Rodrigo' ,}, 
-    { nome :'Alexandre' ,},
+    { id:1 , nome :'Lauro'    , disciplina: 'Matemática'}, 
+    { id:2 , nome :'Roberto'  , disciplina: 'Física'},
+    { id:3 , nome :'Ronaldo'  , disciplina: 'Português'},
+    { id:4 , nome :'Rodrigo'  , disciplina: 'Inglês'}, 
+    { id:5 , nome :'Alexandre', disciplina: 'Programação'},
     ]
 
   constructor() { }
